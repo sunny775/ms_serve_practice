ce9c0b9cb96dff25ccd771299083746185cd0fb8
Sunny Cletus <scletus@gmail.com>
2020-10-02 15:56:46 +0100

    first

--- app.js
+++ app.js
@@ -18,9 +18,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
